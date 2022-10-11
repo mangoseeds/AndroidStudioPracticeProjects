340e5942ce248d4791d51df9c88c958072cb6d1f
J Bae <juyonbae@gmail.com>
2022-10-11 13:55:53 -0400

    project 3 completed: switching images with button click

---
+++ app/src/main/java/onoma/project3/MainActivity.java
@@ -0,0 +1,44 @@
+package onoma.project3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    Button button1;
+    Button button2;
+    ImageView image1;
+    ImageView image2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+
+        image1 = findViewById(R.id.imageView1);
+        image2 = findViewById(R.id.imageView2);
+        image2.setVisibility(View.INVISIBLE);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                image1.setVisibility(View.VISIBLE);
+                image2.setVisibility(View.INVISIBLE);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                image1.setVisibility(View.INVISIBLE);
+                image2.setVisibility(View.VISIBLE);
+            }
+        });
+
+    }
+}
